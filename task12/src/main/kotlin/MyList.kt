69a58f172b2ee6bd94c1058f1b3b468f761ff666
Миша Дубинин <m.dubinin1@g.nsu.ru>
2022-12-26 06:45:07 +0700

    change logic of the solution

--- task12/src/main/kotlin/MyList.kt
+++ task12/src/main/kotlin/MyList.kt
@@ -1,52 +1,41 @@
 import java.util.concurrent.locks.ReentrantLock
 
 class MyList<T : Comparable<T>> {
-    private data class Node<T>(var value: T, val next: Node<T>?, var mutex: ReentrantLock)
+    private data class Node<T>(var value: T, val next: Node<T>?)
+//    private val mutex = ReentrantLock() another way of implementation
 
     @Volatile
     private var head: Node<T>? = null
 
     fun add(forAdd: T) {
-        head = Node(forAdd, head, ReentrantLock())
+        head = Node(forAdd, head)
     }
 
     fun print() {
-        var it = head;
-        while (it != null) {
-            it.mutex.lock()
-            try {
+        synchronized(this) {
+            var it = head
+            while (it != null) {
                 println(it.value)
-            } finally {
-                it.mutex.unlock()
                 it = it.next
             }
         }
     }
 
     fun sort() {
-        var i = head
-        while (i != null) {
-            i.mutex.lock()
-            try {
+        synchronized(this) {
+            var i = head
+            while (i != null) {
                 var j = i.next
                 while (j != null) {
-                    j.mutex.lock()
-                    try {
-                        if (i.value > j.value) {
-                            val tmp = i.value
-                            i.value = j.value
-                            j.value = tmp
-                        }
-                    } finally {
-                        j.mutex.unlock()
-                        j = j.next
+                    if (i.value > j.value) {
+                        val tmp = i.value
+                        i.value = j.value
+                        j.value = tmp
                     }
+                    j = j.next
                 }
-            } finally {
-                i.mutex.unlock()
                 i = i.next
             }
         }
-
     }
 }
